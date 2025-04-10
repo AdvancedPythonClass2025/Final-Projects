384ee9116af1690bdd355040eab27627e35e43c6
reza-we <s.reza.we@gmail.com>
2025-04-10 22:27:03 +0330

    Adding a like function for the like button.

--- Group-1/frontend/app.js
+++ Group-1/frontend/app.js
@@ -4,10 +4,23 @@ function PlayPause(){
     const newSrc = 'icons/pause.png'; 
 
 
-      if (image.src.includes(originalSrc)) {
-            image.src = newSrc; 
-      } else {
-            image.src = originalSrc; 
-        }
+    if (image.src.includes(originalSrc)) {
+        image.src = newSrc; 
+    } else {
+        image.src = originalSrc; 
+    }
     
+}
+
+function like(){
+    const image = document.getElementById('like');
+    const originalSrc = 'icons/like.png'; 
+    const newSrc = 'icons/like_clicked.png'; 
+
+
+    if (image.src.includes(originalSrc)) {
+        image.src = newSrc; 
+    } else {
+        image.src = originalSrc; 
+    }
 }
