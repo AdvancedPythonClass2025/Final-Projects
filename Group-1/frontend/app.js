b158501bc6a6417d1065b0c958834f8b1decb45b
reza-we <s.reza.we@gmail.com>
2025-04-10 22:28:37 +0330

    Adding a dislike function for the dislike button.

--- Group-1/frontend/app.js
+++ Group-1/frontend/app.js
@@ -24,3 +24,16 @@ function like(){
         image.src = originalSrc; 
     }
 }
+
+function dislike(){
+    const image = document.getElementById('dislike');
+    const originalSrc = 'icons/dislike.png'; 
+    const newSrc = 'icons/dislike_clicked.png'; 
+
+
+    if (image.src.includes(originalSrc)) {
+        image.src = newSrc; 
+    } else {
+        image.src = originalSrc; 
+    }
+}
