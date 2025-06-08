b36d615ee6291f8fca846e9712835d10a0c1319d
Radinmrd <radin.morshed323@gmail.com>
2025-06-08 07:18:31 +0330

    Update app.py
    bakhsh entekhab folder

--- Group-1/backend/app.py
+++ Group-1/backend/app.py
@@ -1,1 +1,17 @@
+import tkinter as tk
+from tkinter import filedialog
+
+class MusicPlayer:
+    def __init__(self):
+        self.folder_path = self.choose_folder()
+
+    def choose_folder(self):
+        root = tk.Tk()
+        root.withdraw()
+        folder_selected = filedialog.askdirectory(title="Select Music Folder")
+        return folder_selected if folder_selected else ""
+
+if __name__ == "__main__":
+    player = MusicPlayer()
+    print(f"Selected Folder: {player.folder_path}")
 
