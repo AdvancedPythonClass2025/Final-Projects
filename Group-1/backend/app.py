91990e302a5d4aaaaf453eed4271deeec2233a0f
Radinmrd <radin.morshed323@gmail.com>
2025-06-08 07:19:23 +0330

    Update app.py
    load kardan mp3 ha az folder

--- Group-1/backend/app.py
+++ Group-1/backend/app.py
@@ -1,9 +1,11 @@
+import os
 import tkinter as tk
 from tkinter import filedialog
 
 class MusicPlayer:
     def __init__(self):
         self.folder_path = self.choose_folder()
+        self.playlist = self.load_music_files()
 
     def choose_folder(self):
         root = tk.Tk()
@@ -11,7 +13,9 @@ class MusicPlayer:
         folder_selected = filedialog.askdirectory(title="Select Music Folder")
         return folder_selected if folder_selected else ""
 
+    def load_music_files(self):
+        return [os.path.join(self.folder_path, f) for f in os.listdir(self.folder_path) if f.endswith(".mp3")] if self.folder_path else []
+
 if __name__ == "__main__":
     player = MusicPlayer()
-    print(f"Selected Folder: {player.folder_path}")
-
+    print("Music files found:", player.playlist)
